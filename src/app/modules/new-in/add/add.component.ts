2f0dc462cd80747cdfe013ffcf061153acd711a4
birender <birenderk935@gmail.com>
2025-12-22 17:17:12 +0530

    revert old code

--- src/app/modules/new-in/add/add.component.ts
+++ src/app/modules/new-in/add/add.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 import { ThemePalette } from '@angular/material/core';
 import { ProductService } from 'src/app/services/product.service';
 
@@ -19,13 +19,13 @@ getAllProduct:any=[];
 dataFilter:any;
 isError="";
 category:any[] = [];
-createPro:any={
+createPro={
   productName:'',
-  productQuantity:null,
-  productCategory:'',
   productPurchasingPrice:null,
   productSellingPrice:null,
+  productQuantity:null,
   productPurchasingDate:'',
+  productCategory:''
  
 }
 
@@ -89,7 +89,6 @@ ngOnInit(){
    this.getProductType(); 
    this.dataFilter=this.gettype();
    this.getCategroy()
-  //  this.startCamera()
 }
 ngDoCheck(){
   this.dataFilter=this.gettype();
@@ -102,7 +101,7 @@ selectedFile!: File;
 
 uploadProductImage(event:any){
  this.selectedFile = event.target.files[0];
- if(this.selectedFile.type=='image/png' || this.selectedFile.type=='image/jpeg' || this.selectedFile.type=='image/jpg'){
+
     // ==== Local Preview Code ====
     const reader = new FileReader();
     console.log(reader)
@@ -110,114 +109,46 @@ uploadProductImage(event:any){
       this.previewUrl = reader.result; // base64
     };
     reader.readAsDataURL(this.selectedFile);
-  }else{
-    alert('Please select a valid image file');
-  }
+
 }
 isImage(url: string | null): boolean {
   if (!url) return false;
   return url.startsWith('data:image');
 }
 
+formData = new FormData();
 
 createProduct2(form:any):void{
- const preparedFormData = this.validationAndReadyForm();
- console.log(preparedFormData)
-  this.isFullPageLoad=true
-  this._product.createProduct(preparedFormData).subscribe((res:any)=>{
-    if(res.data){
-    this.isFullPageLoad=false
-    this.isCreatePro=true;
-    setTimeout(() => {
-      this.isCreatePro = false;
-    }, 3000);
+  this.isError = '';
 
-  }},(error)=>{
-    this.isError=error.error.message
-    this.isFullPageLoad=false
-  })
-   this.createProEmpty();
-}
-
-validationAndReadyForm(){
-  
-  if(!this.selectedFile){
-  alert("Please select an image file!");
-  return;
-}
-
-  for (let key in this.createPro) {
-  if (this.createPro[key] === '' || this.createPro[key] === null || this.createPro[key] === undefined) {
-    alert(key + ' is empty');
-    return;
-  }
-}
-  const formData = new FormData();
- formData.append("file", this.selectedFile);
- formData.append("productName", this.createPro.productName);
- formData.append("productPurchasingPrice", String(this.createPro.productPurchasingPrice));
- formData.append("productSellingPrice", String(this.createPro.productSellingPrice));
- formData.append("productQuantity", String(this.createPro.productQuantity));
- formData.append("productPurchasingDate", this.createPro.productPurchasingDate);
- formData.append("productCategory", this.createPro.productCategory);
-  return formData;
-}
-
-
- @ViewChild('video') video!: ElementRef;
-  @ViewChild('canvas') canvas!: ElementRef;
-
-  capturedImage: string | null = null;
-  stream!: MediaStream;
-  useFrontCamera = true;
-
-
-
-  startCamera() {
-    const constraints = {
-      video: {
-        facingMode: this.useFrontCamera ? 'user' : 'environment'
-      }
-    };
-
-    navigator.mediaDevices.getUserMedia(constraints)
-      .then(stream => {
-        this.stream = stream;
-        this.video.nativeElement.srcObject = stream;
-      })
-      .catch(err => {
-        console.error('Camera error:', err);
-      });
-  }
-
-  switchCamera() {
-    this.useFrontCamera = !this.useFrontCamera;
-    this.stopCamera();
-    this.startCamera();
-  }
-
-  capture() {
-    const video = this.video.nativeElement;
-    const canvas = this.canvas.nativeElement;
-
-    canvas.width = video.videoWidth;
-    canvas.height = video.videoHeight;
-
-    const ctx = canvas.getContext('2d');
-    ctx.drawImage(video, 0, 0);
-
-    this.capturedImage = canvas.toDataURL('image/png');
-  }
-
-  stopCamera() {
-    if (this.stream) {
-      this.stream.getTracks().forEach(track => track.stop());
+    if(form.invalid) {
+      this.isError = 'Please fill all required fields!';
+      return;
     }
+  this.formData.append("productName", this.createPro.productName);
+  this.formData.append("productPurchasingPrice", String(this.createPro.productPurchasingPrice));
+  this.formData.append("productSellingPrice", String(this.createPro.productSellingPrice));
+  this.formData.append("productQuantity", String(this.createPro.productQuantity));
+  this.formData.append("productPurchasingDate", this.createPro.productPurchasingDate);
+  this.formData.append("productCategory", this.createPro.productCategory);
+  if (this.selectedFile) {
+    this.formData.append("file", this.selectedFile);
   }
+  console.log(this.formData)
+  // this.isFullPageLoad=true
+  // this._product.createProduct(this.createPro).subscribe((res:any)=>{
+  //   if(res.data){
+  //   this.isFullPageLoad=false
+  //   this.isCreatePro=true;
+  //   setTimeout(() => {
+  //     this.isCreatePro = false;
+  //   }, 3000);
 
-  ngOnDestroy() {
-    this.stopCamera();
-  }
-
+  // }},(error)=>{
+  //   this.isError=error.error.message
+  //   this.isFullPageLoad=false
+  // })
+  //  this.createProEmpty();
 }
 
+}
